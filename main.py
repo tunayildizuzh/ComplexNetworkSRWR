c02c89a7fabd0ebfb2cc7d7b26c38676ee23325e
tunayildizuzh <erdemtuna.yildiz@uzh.ch>
2022-12-30 21:27:46 +0300

    Single Walk to target node implemented

--- main.py
+++ main.py
@@ -3,31 +3,35 @@ import matplotlib.pyplot as plt
 import random
 from randomwalker import SingleSimulation
 from randomwalker import RandomWalker
+import scipy
 
 
 def set_watts_strogatz(n, k, p): # Create a Watts Strogatz Network and assign signs to its edges.
     # G.edges[0, 1]['sign']
     edge_labels = {}
+    node_labels = {}
     G = nx.watts_strogatz_graph(n, k, p)
 
     for i,x,attr in G.edges(data=True):
         val = random.uniform(0,1)
         if val <= 0.5:
             G.add_edge(i,x, sign = '+')
+
         else:
             G.add_edge(i,x,sign = '-')
 
         edge_labels[i,x] = attr['sign']
 
+    nx.set_node_attributes(G,1,'r')
+
     return G, edge_labels
 
-def plot_G():
-    a = set_watts_strogatz(10,6,0.6)
-    G = a[0]
-    pos = nx.spring_layout(G)
-    nx.draw_networkx(G,pos,with_labels=True)
-    nx.draw_networkx_edge_labels(G,pos, edge_labels = a[1], font_color = 'red', font_weight='bold')
-    plt.show()
 
-single_simulation = SingleSimulation(RandomWalker(0,'+',0.15),set_watts_strogatz(20,8,0.4)[0],0,10)
-single_simulation.run()
+graph = set_watts_strogatz(7,5,0.05)
+single_simulation = SingleSimulation(RandomWalker(0,'+',0.1),graph[0],0,5)
+pos = nx.spring_layout(single_simulation.G)
+nx.draw_networkx(single_simulation.G,pos,with_labels=True)
+nx.draw_networkx_edge_labels(single_simulation.G,pos,edge_labels=graph[1],font_color='red',font_weight='bold')
+single_simulation.run(4)
+
+plt.show()
--- randomwalker.py
+++ randomwalker.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 
 class RandomWalker:
@@ -54,44 +56,95 @@ class SingleSimulation:
         self.G = G
         self.t_start = t_start
         self.t_end = t_end
+        self.path = []
 
 
 
     def get_next_event(self):
 
         next_pos = random.choice(list(self.G.neighbors(self.random_walker.position)))
+
         if next_pos == self.random_walker.position:
             self.get_next_event()
 
         return next_pos
 
-    def run(self):
-        pos_init = self.random_walker.position
+    def calculate_r(self,seed_node, current_position, next_position, edge_sign, walker_sign, degree):
 
-        while self.t_start <= self.t_end:
+        # For r+ Case
+        if edge_sign == '+':
+            if walker_sign == '+':
+                self.G.nodes[next_position]['r'] += (1-self.random_walker.c) * (self.G.nodes[current_position]['r'] / degree)
+                print(self.G.nodes[next_position]['r'])
+
+                if seed_node == next_position:
+                    self.G.nodes[next_position]['r'] += (1 - self.random_walker.c) * (self.G.nodes[current_position]['r'] / degree) + 1
+                    print(self.G.nodes[next_position]['r'])
+
+        if edge_sign == '-':
+            if walker_sign == '-':
+                self.G.nodes[next_position]['r'] += (1 - self.random_walker.c) * (self.G.nodes[current_position]['r'] / degree)
+                print(self.G.nodes[next_position]['r'])
+
+                if seed_node == next_position:
+                    self.G.nodes[next_position]['r'] += (1 - self.random_walker.c) * (self.G.nodes[current_position]['r'] / degree) + 1
+                    print(self.G.nodes[next_position]['r'])
+
+        # For r- Case
+        if edge_sign == '+':
+            if walker_sign == '-':
+                self.G.nodes[next_position]['r'] -= (1 - self.random_walker.c) * (self.G.nodes[current_position]['r'] / degree)
+                print(self.G.nodes[next_position]['r'])
+
+        if edge_sign == '-':
+            if walker_sign == '+':
+                self.G.nodes[next_position]['r'] -= (1 - self.random_walker.c) * (self.G.nodes[current_position]['r'] / degree)
+                print(self.G.nodes[next_position]['r'])
+
+    def run(self,target_node):
+        visited = []
+        pos_init = self.random_walker.position
+        visited.append(pos_init)
+        idx = 0
+        # while self.t_start <= self.t_end:
+
+        while target_node != self.random_walker.position:
 
             reset_prop = random.uniform(0,1)
             if reset_prop < self.random_walker.c: # Reset Case.
                 self.random_walker.position = pos_init
                 self.random_walker.sign = '+'
                 print(f'Reset. Current position: {pos_init}')
+                visited = [visited[0]]
 
             else: # Jump Case.
 
                 next_position = self.get_next_event()
-                if self.G.edges[self.random_walker.position, next_position]['sign'] == '-':
-                    if self.random_walker.sign == '+':
-                        self.random_walker.sign = '-'
-                    elif self.random_walker.sign == '-':
-                        self.random_walker.sign = '+'
 
-                print(f"From: {self.random_walker.position}, To: {next_position}, Edge Sign: {self.G.edges[self.random_walker.position, next_position]['sign']} ")
-                self.random_walker.position = next_position
+                if next_position not in visited:
+                    visited.append(next_position)
+                    # (self,seed_node, current_position, next_position, edge_sign, walker_sign, degree):
+                    self.calculate_r(0,self.random_walker.position,next_position,self.G.edges[self.random_walker.position, next_position]['sign'],self.random_walker.sign,self.G.degree(self.random_walker.position))
 
+                    if self.G.edges[self.random_walker.position, next_position]['sign'] == '-':
+                        if self.random_walker.sign == '+':
+                            self.random_walker.sign = '-'
+                        elif self.random_walker.sign == '-':
+                            self.random_walker.sign = '+'
+                    elif self.G.edges[self.random_walker.position, next_position]['sign'] == '+':
+                        if self.random_walker.sign == '+':
+                            self.random_walker.sign = '+'
+                        elif self.random_walker.sign == '-':
+                            self.random_walker.sign = '-'
 
-            self.t_start += 1
-            print(f'Current Sign: {self.random_walker.sign}')
+                    # self.calculate_r(0,self.random_walker.position,next_position,self.G.edges[self.random_walker.position, next_position]['sign'],self.random_walker.sign,self.G.degree(self.random_walker.position))
 
+                    print(f"From: {self.random_walker.position}, To: {next_position}, Edge Sign: {self.G.edges[int(self.random_walker.position), int(next_position)]['sign']} ")
+                    self.random_walker.position = next_position
+                    self.t_start += 1
+                else:
+                    continue
+                print(f'SRWR Sign: {self.random_walker.sign}')
 
 
 
