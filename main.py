4f84f1e6535ce52c78edef7a2d12ca3c6ced6364
tunayildizuzh <erdemtuna.yildiz@uzh.ch>
2023-01-23 11:46:43 +0800

    Heatmap implemented.

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 from randomwalker import Simulation
+import numpy as np
 import scipy
 from readdata import set_G
+import pandas as pd
+import seaborn as sns
 from collections import OrderedDict
 
 
@@ -32,12 +35,27 @@ def set_watts_strogatz(n, k, p): # Create a Watts Strogatz Network and assign si
     # print(f'edgelabels: {edge_labels}')
     return G, edge_attr
 
-def comparison_dict(seed_node):
+# def comparison_dict(seed_node):
+#     comparison = {}
+#     rd = sim2.iterate(sim2.normalize()[0], sim2.normalize()[1], graph2[2])[0] - \
+#          sim2.iterate(sim2.normalize()[0], sim2.normalize()[1], graph2[2])[1]
+#     for idx, val in enumerate(sim2.nodes):
+#         comparison[seed_node,val] = rd[idx]
+#
+#     for a,b in comparison:
+#         if comparison[a,b] > 0:
+#             comparison[a,b] = '+'
+#         else:
+#             comparison[a,b] = '-'
+#
+#
+#     return comparison
+
+def comparison_dict2(seed_node,rd_val,graph_nodes):
     comparison = {}
-    rd = sim2.iterate(sim2.normalize()[0], sim2.normalize()[1], graph2[2])[0] - \
-         sim2.iterate(sim2.normalize()[0], sim2.normalize()[1], graph2[2])[1]
-    for idx, val in enumerate(sim2.nodes):
-        comparison[seed_node,val] = rd[idx]
+
+    for idx, val in enumerate(graph_nodes):
+        comparison[seed_node,val] = rd_val[idx]
 
     for a,b in comparison:
         if comparison[a,b] > 0:
@@ -48,13 +66,27 @@ def comparison_dict(seed_node):
 
     return comparison
 
-def accuracy():
+# def accuracy():
+#     count = 0
+#     true = 0
+#
+#     comparison = comparison_dict(graph2[2])
+#     for a,b in edge_attributes:
+#             if a == graph2[2]:
+#                 count+=1
+#                 if edge_attributes[a,b] == comparison[a,b]:
+#                     true+=1
+#     accuracy_percentage = (true/count) * 100
+#
+#     return accuracy_percentage
+
+def accuracy2(seed_node,rd_val,graph_nodes,edge_attributes):
     count = 0
     true = 0
 
-    comparison = comparison_dict(graph2[2])
+    comparison = comparison_dict2(seed_node,rd_val,graph_nodes)
     for a,b in edge_attributes:
-            if a == graph2[2]:
+            if a == seed_node:
                 count+=1
                 if edge_attributes[a,b] == comparison[a,b]:
                     true+=1
@@ -94,59 +126,59 @@ Part 1: Working with a synthetic network to check if SRWR works.
 """
 Part 2: Use a real network for SRWR application.
 """
-graph2 = set_G()
+# graph2 = set_G()
+#
+# print(f'Seed Node: {graph2[2]}')
+# sim2 = Simulation(graph2[0],seed_node=graph2[2],c=0.15,beta=0.3,gamma=0.5,epsilon=0.1)
+# edge_attributes = nx.get_edge_attributes(sim2.G,'sign')
+# # print(f"Initial State: {edge_attributes}")
+# # print(sim2.nodes)
+# # print(f'edges:{graph2[4]}')
+#
+# sim2.normalize()
+# sim2.iterate(sim2.normalize()[0],sim2.normalize()[1],graph2[2])
+# # sim2.remove_edge(node=graph2[2])
+# # sim2.remove_edge(node=graph2[2])
+# edge_attr = nx.get_edge_attributes(sim2.G,'sign')
+# rd = sim2.iterate(sim2.normalize()[0],sim2.normalize()[1],graph2[2])[0] - sim2.iterate(sim2.normalize()[0],sim2.normalize()[1],graph2[2])[1]
+# zipped_values = list(zip(sim2.nodes,rd))
+# # print(f'ZIPPED: {zipped_values}')
+# # edge_attr_sim2 = nx.get_edge_attributes(sim2.G,'sign')
+# # print(f"Edge Attributes: {edge_attr_sim2}")
+# print(f"Accuracy Percentage: {accuracy()}%")
+# # pos = nx.spring_layout(sim2.G)
+# # nx.draw_networkx(sim2.G,pos,with_labels=True)
+# # nx.draw_networkx_edge_labels(sim2.G,pos,edge_labels=edge_attr,font_color='red',font_weight='bold')
+# # plt.show()
 
-print(f'Seed Node: {graph2[2]}')
-sim2 = Simulation(graph2[0],seed_node=graph2[2],c=0.15,beta=0.3,gamma=0.5,epsilon=0.1)
-edge_attributes = nx.get_edge_attributes(sim2.G,'sign')
-# print(f"Initial State: {edge_attributes}")
-# print(sim2.nodes)
-# print(f'edges:{graph2[4]}')
 
-sim2.normalize()
-sim2.iterate(sim2.normalize()[0],sim2.normalize()[1],graph2[2])
-# sim2.remove_edge(node=graph2[2])
-# sim2.remove_edge(node=graph2[2])
-edge_attr = nx.get_edge_attributes(sim2.G,'sign')
-rd = sim2.iterate(sim2.normalize()[0],sim2.normalize()[1],graph2[2])[0] - sim2.iterate(sim2.normalize()[0],sim2.normalize()[1],graph2[2])[1]
-zipped_values = list(zip(sim2.nodes,rd))
-# print(f'ZIPPED: {zipped_values}')
-# edge_attr_sim2 = nx.get_edge_attributes(sim2.G,'sign')
-# print(f"Edge Attributes: {edge_attr_sim2}")
-print(f"Accuracy Percentage: {accuracy()}%")
-pos = nx.spring_layout(sim2.G)
-# nx.draw_networkx(sim2.G,pos,with_labels=True)
-# nx.draw_networkx_edge_labels(sim2.G,pos,edge_labels=edge_attr,font_color='red',font_weight='bold')
-# plt.show()
+def run(beta, gamma, epsilon): # 0.3,0.5,0.1
+    graph_network = set_G()
+    print(f'Seed Node: {graph_network[2]}')
+    simulate = Simulation(graph_network[0],seed_node=graph_network[2],c=0.15,beta=beta,gamma=gamma,epsilon=epsilon)
+    edge_attributes = nx.get_edge_attributes(simulate.G, 'sign')
+    simulate.normalize()
+    simulate.iterate(simulate.normalize()[0],simulate.normalize()[1],graph_network[2])
+    rd = simulate.iterate(simulate.normalize()[0], simulate.normalize()[1], graph_network[2])[0] - simulate.iterate(simulate.normalize()[0], simulate.normalize()[1], graph_network[2])[1]
+
+    print(f'Accuracy Percentage: {accuracy2(graph_network[2],rd,simulate.nodes,edge_attributes)}%')
+    return accuracy2(graph_network[2],rd,simulate.nodes,edge_attributes)
 
 
-def run():
-    graph2 = set_G()
+parameter_values = np.arange(0.1,1.1,0.1)
+accuracies = np.zeros((10,10))
 
-    print(f'Seed Node: {graph2[2]}')
-    sim2 = Simulation(graph2[0], seed_node=graph2[2], c=0.15, beta=0.3, gamma=0.5, epsilon=0.1)
-    edge_attributes = nx.get_edge_attributes(sim2.G, 'sign')
-    # print(f"Initial State: {edge_attributes}")
-    # print(sim2.nodes)
-    # print(f'edges:{graph2[4]}')
+for index,value in enumerate(parameter_values): # X = beta Y = Gamma
+    for index2,value2 in enumerate(parameter_values):
+        accuracies[index,index2] = run(value,value2,0.1)
 
-    sim2.normalize()
-    sim2.iterate(sim2.normalize()[0], sim2.normalize()[1], graph2[2])
-    # sim2.remove_edge(node=graph2[2])
-    # sim2.remove_edge(node=graph2[2])
-    edge_attr = nx.get_edge_attributes(sim2.G, 'sign')
-    rd = sim2.iterate(sim2.normalize()[0], sim2.normalize()[1], graph2[2])[0] - \
-         sim2.iterate(sim2.normalize()[0], sim2.normalize()[1], graph2[2])[1]
-    zipped_values = list(zip(sim2.nodes, rd))
-    # print(f'ZIPPED: {zipped_values}')
-    # edge_attr_sim2 = nx.get_edge_attributes(sim2.G,'sign')
-    # print(f"Edge Attributes: {edge_attr_sim2}")
-    print(f"Accuracy Percentage: {accuracy()}%")
-    pos = nx.spring_layout(sim2.G)
-    # nx.draw_networkx(sim2.G,pos,with_labels=True)
-    # nx.draw_networkx_edge_labels(sim2.G,pos,edge_labels=edge_attr,font_color='red',font_weight='bold')
-    # plt.show()
+print(accuracies)
 
+df = pd.DataFrame(accuracies)
+
+ax = sns.heatmap(df, annot = True)
+ax.set(xlabel='Beta',ylabel='Gamma')
+plt.show()
 
 
 
